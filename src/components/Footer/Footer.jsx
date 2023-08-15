f1cbeb7707825a90c54234f50b81e5eb1ef9ac19
Rifki Ramadhan <riifkiramadhan2@gmail.com>
2023-08-15 14:13:29 +0700

    Rename footer text

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css';
 const Footer = () => {
   return (
     <div className='footer'>
-      Copyright © 2023 Tokopedia Play Clone #GenarasiGIGH 3.0
+      Copyright © 2023 Tokopedia Play Clone #GenerasiGIGH 3.0
     </div>
   );
 };
